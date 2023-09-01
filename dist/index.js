eb92ef7a5e5407ecacc39a13f66e24b513e824e2
dylan <106468433+mroxlong@users.noreply.github.com>
2023-09-01 20:46:10 +0200

    update

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Floopi = void 0;
+class Floopi {
+}
+exports.Floopi = Floopi;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+class Floopi{
+
+}
+
+export { Floopi }
